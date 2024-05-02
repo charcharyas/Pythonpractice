f376c33186ef87fff1d4a14bd7debcc48c7a6711
charcharyas <hillc@wsc.school.nz>
2024-05-02 12:28:06 +1200

    made quiz v1

---
+++ main.py
@@ -0,0 +1,15 @@
+#Ask the user their name and save it
+name=input("Hey, what is your name")
+#Greet the user and introduce the quiz
+print ("hello" ,name, "welcome to the game show where we awnser questions")
+#Ask the user a question
+awnser=input("So, whats the capital of france")
+
+#Tell them the correct answer
+if awnser == " Paris" or " paris" or " hell":
+    print("goodjob! you got it right hope you enjoyed playing!")   #End the quiz
+else:
+     print("... \n... \n... \n... \n... \n... \n... \n... \n... \n... \n... \n... \n... \n... \n... \n... \n... \n... \n... \n... \n... \n... \n... \n... \n... \n... \n... \n... \n Are you American?")
+
+
+      
