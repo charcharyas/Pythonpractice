71d8ec8cd52b5df0faff31bef113c8ec3f8ac615
charcharyas <hillc@wsc.school.nz>
2024-05-17 12:59:27 +1200

    Update main.py
    went back and worked on quiz 3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #Ask the user their name and save it
+QUESTIONFORMAT= "{}\na. {}\nb. {}\nc. \n"
 play ="yes"
 score =0
 name=input("Hey, what is your name")
@@ -15,29 +16,41 @@ while play =="yes":
           except: 
                print ("errrrrrrrrrrrrm what the filp buddy... that wasn't a numper pal. D:")
      score =0
-     awnser=input("So, whats the capital of france")
+     
 
      #Tell them the correct answer
-     if awnser == " Paris":
-          score +=1
-          print ("goodjob! you awnserd paris your score is now {}!" .format(score))
-     elif awnser == "":
-          print ("why aint you awnsering pookie, i know its you henry")  #End the quiz
-     else:
-          score -=1
-          print("... \n Are you American? also your score is now",score, )
+     attemps=tries
+     while attemps > 0:
+          awnser=input("So, whats the capital of france")
+          if awnser == " Paris":
+               score +=1
+               print ("goodjob! you awnserd paris your score is now {}!" .format(score))
+               break
+          elif awnser == "":
+               print ("why aint you awnsering pookie, i know its you henry")  #End the quiz
+               attemps -= 1
+          else:
+               score -=1
+               print("... \n Are you American? also your score is now",score, )
+               attemps -= 1
      
-     awnser =input("So now, how many tentacles does a 7 armed octopus have. is it \nA.8\nB.7\nC.4 ")
-     if awnser == "A":
-          score +=1
-          print ("goodjob! you awnserd 8 your score is now {}!" .format(score))
-     elif awnser == "":
-          print ("why aint you awnsering pookie, i know its you henry")
-     elif awnser != "A" or awnser != "B" or awnser != "C":
-          print("thats not an awnser buddy")
-     else:
-          score -=1
-          print("... \n Nah man its an octopus of course its gonna be 8 duh. also you now have a total score of",score,)
+     attemps=tries
+     while attemps > 0:
+          questions = "So now, how many tentacles does a 7 armed octopus have."
+          a= 8
+          b= 7
+          c= 4
+          awnser =input(" {}\nA. {} B. {} C {}}" QUESTIONFORMAT.format (questions, a, b, c)).lower()
+          if awnser == a or awnser== "a":
+               score +=1
+               print ("goodjob! you awnserd 8 your score is now {}!" .format(score))
+          elif awnser == "":
+               print ("why aint you awnsering pookie, i know its you henry")
+          elif awnser != "a" or awnser != "a" or awnser != "a":
+               print("thats not an awnser buddy")
+          else:
+               score -=1
+               print("... \n Nah man its an octopus of course its gonna be 8 duh. also you now have a total score of",score,)
 
      play == input("Wanna play again")
      
