d5ec0e216720f8c06ef6e21dcf59b050ee4bfcb5
charcharyas <hillc@wsc.school.nz>
2024-06-02 11:38:10 +1200

    finished quiz 5

--- main.py
+++ main.py
@@ -1,62 +1,67 @@
 #Ask the user their name and save it
 import random
-goodc= ["good job gang", "if you were robert i would make out with you(you got it right)", "Jeffery epstien would be proud of you"]
-badc= ["...\n are you american?", "your a bad kitten pookie", "GRRRRRRR YOU GOT IT WRONG YA TWINK"]
-QUESTIONS = ["what is the capital of France",
-           "how many tentacles does a 7 armed octopus have"]
-OPTIONS = [["paris", "Paris", "Berlin"],
-           ["8", "7", "6"]]
-SHORTOPTION =["a", "b", "c"]
-AWNSER = [2,1]
 
+good_comments = [
+    "Good job, gang!",
+    "If you were Robert, I would make out with you (you got it right)",
+    "Jeffrey Epstein would be proud of you"
+]
 
-QUESTIONFORMAT= "{}\na. {}\nb. {}\nc. {} \n"
-play ="yes"
-score =0
-name=input("Hey, what is your name")
-#Greet the user and introduce the quiz
+bad_comments = [
+    "...\n Are you American?",
+    "You're a bad kitten, pookie",
+    "GRRRRRR, YOU GOT IT WRONG!!!"
+]
 
-#Ask the user a question
+questions = [
+    "What is the capital of France?",
+    "How many tentacles does a 7-armed octopus have?"
+]
 
-while play =="yes":
-     while True:
-          try:
-               tries = input("hello {} welcome to the game show where we awnser questions \n how many attemts do you want" .format(name))
-               tries = int(tries)
-               break
-          except: 
-               print ("errrrrrrrrrrrrm what the filp buddy... that wasn't a number pal. D:")
-     score =0
-<<<<<<< HEAD
-     #Tell them the correct answer
+options = [
+    ["paris", "Berlin", "Paris"],
+    ["8", "7", "6"]
+]
 
-     score==0
+short_options = ["a", "b", "c"]
+answers = [2,0]
 
-          for i in range(len(QUESTIONS)):
-     attemps=tries
-     while attemps > 0:
-          awnser = input(QUESTIONFORMAT.format(QUESTIONS[i], OPTIONS[i][0], OPTIONS[i][1], OPTIONS[i][2])).lower()
-          if awnser == OPTIONS[0][AWNSER[0]] or awnser == SHORTOPTION[AWNSER[0]]:
-          print(random.choice (goodc))
-     elif awnser in SHORTOPTION or awnser in OPTIONS[0]:
-          print(random.choice (badc))
-     else
-          print("wrong!")
-          print("That wasn't an awnser pal.\nGRRRRRRR daddy is now mad at you")
-play == input("Wanna play again")
-=======
-     #Tell them the correct answe
-     for i in range(len(QUESTIONS)):
-          attemps=tries
-          while attemps > 0:
-               awnser = input(QUESTIONFORMAT.format(QUESTIONS[i], OPTIONS[i][0], OPTIONS[i][1], OPTIONS[i][2])).lower()
-               if awnser == OPTIONS[i][AWNSER[i]] or awnser == SHORTOPTION[AWNSER[i]]:
-                    print(random.choice (goodc))
-                    break
-               elif awnser in SHORTOPTION or awnser in OPTIONS[i]:
-                  print(random.choice (badc))
-                  print("super rizz")
-               else:    
-                    print("That wasn't an awnser pal.\nGRRRRRRR daddy is now mad at you")
-          play == input("Wanna play again")
->>>>>>> c3490f02653c49216f4f71e3da997aea5051660e
+question_format = "{}\na. {}\nb. {}\nc. {} \n"
+play = "yes"
+
+name = input("Hey, what is your name? ")
+
+while play.lower() == "yes":
+    # Greet the user and introduce the quiz
+    while True:
+        try:
+            tries = input(f"Hello {name}, welcome to the game show where we answer questions!\nHow many attempts do you want? ")
+            tries = int(tries)
+            break
+        except ValueError:
+            print("Errrrrrrrrrrrrm, what the flip buddy... that wasn't a number, pal. D:")
+
+    score = 0
+
+    for i in range(len(questions)):
+        attempts = tries
+        while attempts > 0:
+            answer = input(question_format.format(questions[i], options[i][0], options[i][1], options[i][2])).lower()
+            if answer == options[i][answers[i]].lower() or answer == short_options[answers[i]]:
+                print(random.choice(good_comments))
+                score += 1
+                break
+            elif answer in short_options or answer in options[i]:
+                print(random.choice(bad_comments))
+                score -= 1
+                attempts -= 1
+            elif answer == "":
+                print("Why aren't you answering, pookie?")
+            else:
+                print("That wasn't an answer, pal.\nGRRRRRR, daddy is now mad at you")
+                attempts -= 1
+
+    print("well done {} you got a score of {}".format(name,score))
+    play = input("Wanna play again? (yes/no) ")
+
+print("Thanks for playing!")
