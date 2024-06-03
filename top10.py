5975304527a875af1f26e962a2cd274b393a300f
charcharyas <hillc@wsc.school.nz>
2024-06-04 07:56:16 +1200

    made decent progress on my top 10 quiz

--- top10.py
+++ top10.py
@@ -7,12 +7,21 @@ def getlives():
         lives =input("how many lives do you want")
         try:
             lives = int(lives)
-            if lives >= 0:
+            if lives > 0:
                 return lives
             else:
                 print("Your not cool buddy, I know you may think you are sooooooo smart for putting a number that is 0 or under but did you know that not a single person actually likes you? ")
         except: 
             print ("errrrrrrrrrrrrm what the filp buddy... that wasn't a number pal. D:")
+def getpassword():
+    while True:
+        password = input("Whats the password")
+        if password == ("password"):
+            return
+        else:
+            print ("Nah uh bro, try again")
 #maincode
 intro()
 lives = getlives()
+print("coolio then one more this before we get started")
+getpassword()
